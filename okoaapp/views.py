7a8637f10652467d71ee89f88f0364fcd834b9e1
MIKENYABS <sanitizerheart3@gmail.com>
2024-02-16 16:22:08 +0300

    initial commit

---
+++ okoaapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def images(request):
+    return render(request, 'gallery.html')
+
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+
+def collections(request):
+    return render(request, 'collection.html')
+
+
